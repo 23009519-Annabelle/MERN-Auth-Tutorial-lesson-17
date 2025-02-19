b41cfc242d7b03bae7f21c9e257f88c2e461156e
23009519-Annabelle <23009519@myrp.edu.sg>
2025-02-19 22:41:26 +0800

    C219-MERNapp

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -1,26 +1,26 @@
-const jwt = require('jsonwebtoken')
-const User = require('../models/userModel')
+// In your authMiddleware.js or requireAuth.js file
+const jwt = require('jsonwebtoken');
+const User = require('./models/userModel') // Import your user model
 
 const requireAuth = async (req, res, next) => {
+    // verify authentication
+    const { authorization } = req.headers;
 
-  // verify authentication
-  const { authorization } = req.headers
+    if (!authorization) {
+        return res.status(401).json({ error: 'Authorization token required' });
+    }
 
-  if (!authorization) {
-    return res.status(401).json({error: 'Authorization token required'})
-  }
+    const token = authorization.split(' ')[1]; // "Bearer <token>"
+    
+    try{
+        const {_id} = jwt.verify(token, process.env.SECRET);
+        req.user = await User.findOne({_id}).select('_id');
+        next()
+    }catch(error){
+        console.log(error)
+        res.status(401).json({error:'Request is not authorized'})
+    }
+    
+};
 
-  const token = authorization.split(' ')[1]
-
-  try {
-    const {_id} = jwt.verify(token, process.env.SECRET) //If secret key does not match with the key used for creating token it will throw an error
-
-    req.user = await User.findOne({ _id }).select('_id') // try to get user in db
-    next()
-
-  } catch (error) {
-    console.log(error)
-    res.status(401).json({error: 'Request is not authorized'}) //error is most likely here
-  }
-}
-module.exports = requireAuth
+module.exports = requireAuth;
